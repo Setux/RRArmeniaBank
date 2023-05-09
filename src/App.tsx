ffe579516d5399c2d4828934041a18fc8d5f5b2d
Setux <zaytsev_danya@bk.ru>
2023-05-09 18:09:47 +0300

    linter

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useContext } from "react";
 import { webAppContext } from "./contexts/appContext";
 import { MetroSpinner } from "react-spinners-kit";
 function App() {
-  const app = useContext(webAppContext);
-
   return (
     <div className="App">
       <MetroSpinner size={30} color='#2678b6' loading />
