f9c9869d5ced8fb3a1ada6cad014b830b0f8b629
Setux <zaytsev_danya@bk.ru>
2023-05-09 18:06:14 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import { useContext } from "react";
 import { webAppContext } from "./contexts/appContext";
-
+import { MetroSpinner } from "react-spinners-kit";
 function App() {
   const app = useContext(webAppContext);
-  console.log(app);
 
-  const sendData = () => {
-    app.sendData("Bogdan huesos");
-  };
-  console.log(app.initData)
   return (
     <div className="App">
-      {app.version ? (
-        <div className="layout">
-          <button onClick={sendData}>click me</button>
-          <p>initDataUnsafe</p>
-          <code></code>
-        </div>
-      ) : (
-        "loading"
-      )}
+      <MetroSpinner size={30} color='#2678b6' loading />
     </div>
   );
 }
