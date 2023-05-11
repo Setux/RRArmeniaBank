84aaf1ffa5343c4c4a5ee5bf20b64beda312a20a
Setux <zaytsev_danya@bk.ru>
2023-05-12 00:49:54 +0300

    test layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { Layout, Menu, Button, theme } from 'antd';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
 import Loader from "./components/Loader/Loader";
 import { apiContext } from "./contexts/apiContext";
 import { webAppContext } from "./contexts/appContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
+
+const { Header, Sider, Content } = Layout;
 
 function App() {
   const app = useContext(webAppContext);
   const API = useContext(apiContext);
+
+  const [collapsed, setCollapsed] = useState(false);
+    const {
+      token: { colorBgContainer },
+    } = theme.useToken();
 
   useEffect(() => {
     if (app.initDataUnsafe && API.id) {
@@ -14,7 +29,59 @@ function App() {
   }, [app, API])
 
   return (
-    (app.initDataUnsafe && API.id) ? <Loader isLoading /> : <Loader isLoading />
+    (app.initDataUnsafe && API.id) ?
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: '1',
+                icon: <UserOutlined />,
+                label: 'nav 1',
+              },
+              {
+                key: '2',
+                icon: <VideoCameraOutlined />,
+                label: 'nav 2',
+              },
+              {
+                key: '3',
+                icon: <UploadOutlined />,
+                label: 'nav 3',
+              },
+            ]}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: '16px',
+                width: 64,
+                height: 64,
+              }}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    : <Loader isLoading />
   );
 }
 
