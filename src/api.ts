23d564778481aa2b44ec865683ea27147a0b1312
Setux <zaytsev_danya@bk.ru>
2023-05-11 14:26:05 +0300

    test

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,8 @@ export const getBalance = async (id: number | undefined) => {
     if (!id) {
         return;
     }
-    console.log(id);
-    const responce = await fetch (`https://api.ryodan.dev/getBalance?type=all&id=${id}`);
-    const data = await responce.json()
-    console.log(data);
+    const responce = fetch (`https://api.ryodan.dev/getBalance?type=all&id=${id}`);
+    console.log(responce);
+    // const data = await responce.json()
+    // console.log(data);
 }
