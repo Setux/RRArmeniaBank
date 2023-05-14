bed7c51346a8b53f2e8bc1d0c4e73a64e0e14250
Setux <zaytsev_danya@bk.ru>
2023-05-14 07:53:28 +0300

    balance update

--- src/components/BalanceList/BalanceList.tsx
+++ src/components/BalanceList/BalanceList.tsx
@@ -1,9 +1,14 @@
 import {List, Card} from 'antd';
-// import {GiStonePile, GiOilDrum, GiCutDiamond} from 'react-icons/gi'
-import { FaDollarSign} from 'react-icons/fa'
+import {GiStonePile, GiOilDrum, GiCutDiamond} from 'react-icons/gi'
+import {FaDollarSign, FaRadiation} from 'react-icons/fa'
 import { GetBalanceResponce } from '../../api';
 
-// const RESOURCE_TYPES = [{resource: 'Oil'}, {resource: 'Ore'}, {resource: 'Uranium'}, {resource: 'Diamond'}]
+const RESOURCE_TYPES = [
+    {resource: 'Oil', icon: <GiOilDrum />}, 
+    {resource: 'Ore', icon: <GiStonePile />}, 
+    {resource: 'Uranium', icon: <FaRadiation />}, 
+    {resource: 'Diamond', icon: <GiCutDiamond />}
+]
 
 interface Props {
     balance: GetBalanceResponce<number>;
@@ -15,6 +20,9 @@ const BalanceList = ({balance}: Props) => {
             <List dataSource={[{resource: "$"}]} grid={{column: 1}} renderItem={(item) => {
                 return <List.Item><Card title={<><FaDollarSign/> Money</>}>{balance[item.resource as "$"]}</Card></List.Item>
             }}/>
+            <List dataSource={RESOURCE_TYPES} grid={{column: 2, gutter: 4}} renderItem={(item) => {
+                return <List.Item><Card title={<>{item.icon} {item.resource}</>}>{balance[item.resource.toLowerCase() as 'oil' | 'ore' | 'diamond' | 'uranium']}</Card></List.Item>
+            }} />
         </>
     )
 }
