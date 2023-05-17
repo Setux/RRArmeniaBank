3d19e102d109b08f22dc3e6e5a44c9704cbeb2dc
Setux <zaytsev_danya@bk.ru>
2023-05-17 23:40:10 +0300

    tabs

--- src/pages/OffersPage/OffersPage.tsx
+++ src/pages/OffersPage/OffersPage.tsx
@@ -1,6 +1,15 @@
 import {useContext, useState, useEffect} from 'react';
 import { apiContext } from '../../contexts/apiContext';
 import { OfferResponce } from '../../api';
+import Loader from '../../components/Loader/Loader';
+import { Tabs } from 'antd';
+
+const TAB_PANE = [
+    {label: 'Ore', key: '1', children: 'ORE'},
+    {label: 'Oil', key: '2', children: 'OIL'},
+    {label: 'Uranium', key: '3', children: 'URANIUM'},
+    {label: 'Diamond', key: '4', children: 'Diamond'}
+]
 
 const OffersPage = () => {
     const API = useContext(apiContext);
@@ -19,7 +28,13 @@ const OffersPage = () => {
 
     console.log(offersList)
 
-    return <div>Offers</div>
+    return <div className='offers_container'>{offersList[0].id ?
+        <Tabs
+        defaultActiveKey="1"
+        centered
+        items={TAB_PANE}
+      /> : 
+         <Loader isLoading/>}</div>
 }
 
 export default OffersPage
