5392578a61d0719f3c692a4c0da75c29b11d7e1b
Setux <zaytsev_danya@bk.ru>
2023-05-18 00:44:33 +0300

    fix

--- src/components/OffersList/OffersList.tsx
+++ src/components/OffersList/OffersList.tsx
@@ -5,7 +5,7 @@ const mocked_data = [{amount: 1000, price: 82}, {amount: 100005450, price: 85}, 
 
 const OffersList = () => {
     return (
-        <List pagination={{ position: 'bottom', align: 'center', pageSize={3} }} dataSource={mocked_data} grid={{column: 1, gutter: 4}} renderItem={(item) => {
+        <List pagination={{ position: 'bottom', align: 'center', pageSize: 3 }} dataSource={mocked_data} grid={{column: 1, gutter: 4}} renderItem={(item) => {
             return <List.Item><Card title={<>Resources in stock: {item.amount}</>} actions={[<BsCart4 key="buy"/>]}>Price: {item.price}$/1</Card></List.Item>
         }} />
     )
