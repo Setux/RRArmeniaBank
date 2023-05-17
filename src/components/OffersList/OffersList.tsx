63caa6a3b3724187738ccbc5bdaae89bdfc3afe5
Setux <zaytsev_danya@bk.ru>
2023-05-18 01:16:26 +0300

    fix amount

--- src/components/OffersList/OffersList.tsx
+++ src/components/OffersList/OffersList.tsx
@@ -6,7 +6,7 @@ const mocked_data = [{amount: 1000, price: 82}, {amount: 100005450, price: 85}, 
 const OffersList = () => {
     return (
         <List pagination={{ position: 'bottom', align: 'center', pageSize: 3 }} dataSource={mocked_data} grid={{column: 1, gutter: 4}} renderItem={(item) => {
-            return <List.Item><Card title={<>Resources in stock: {item.amount}</>} actions={[<BsCart4 key="buy"/>]}>Price: {item.price}$/1</Card></List.Item>
+            return <List.Item><Card title={<>Resources in stock: {item.amount.toString().split('').reverse().join('').match(/.{1,3}/g)?.reverse().map(el => el.split('').reverse().join('')).join('.')}</>} actions={[<BsCart4 key="buy"/>]}>Price: {item.price}$/1</Card></List.Item>
         }} />
     )
 }
